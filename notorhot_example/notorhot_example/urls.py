f438f94597c533d14840dcf5411b6605a002f490
Noemi Millman <noemi@strayboots.com>
2013-11-15 16:35:41 -0500

    Sample app skeleton

---
+++ notorhot_example/notorhot_example/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'notorhot_example.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('notorhot_categories'))),
+    url(r'^hot/', include('notorhot.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
