d39b5b538b9384518f761d3d6cce2d4157520681
Noemi Millman <noemi@strayboots.com>
2013-11-19 18:54:49 -0500

    Stub out integration test cases

---
+++ notorhot/_tests/integration.py
@@ -0,0 +1,28 @@
+import datetime
+from mock import Mock, patch
+
+from django.test import TestCase
+from django.forms import ValidationError
+
+from notorhot._tests.factories import mixer
+from notorhot._tests._utils import setup_view, ViewTestMixin
+from notorhot.models import CandidateCategory, Candidate, Competition
+
+class CompetitionViewTestCase(TestCase):
+    pass
+
+
+class VoteViewTestCase(TestCase):
+    pass
+    
+
+class CandidateViewTestCase(TestCase):
+    pass
+    
+
+class LeaderboardViewTestCase(TestCase):
+    pass
+
+
+class CategoryListViewTestCase(TestCase):
+    pass
--- notorhot/tests.py
+++ notorhot/tests.py
@@ -4,6 +4,11 @@ from notorhot._tests.models import NotorHotCategoryTestCase, \
 from notorhot._tests.forms import NotorHotVoteFormTestCase
 from notorhot._tests.views import CompetitionViewTestCase, VoteViewTestCase, \
     CandidateViewTestCase, LeaderboardViewTestCase, CategoryListViewTestCase
+# need non-overlapping names
+from notorhot._tests.integration import CompetitionViewTestCase as CompTestCast, \
+    VoteViewTestCase as VoteTestCase, CandidateViewTestCase as CandTestCase, \
+    LeaderboardViewTestCase as LeadTestCase, \
+    CategoryListViewTestCase as CatTestCase
 
 from notorhot._tests.utils import NeverCacheMixinTestCase, \
     WorkingSingleObjectMixinTestCase, CategoryMixinTestCase, \
