15429c8da3582b42d7aaef12978feb1bd41b82dc
Noemi Millman <noemi@strayboots.com>
2013-11-19 13:56:12 -0500

    Stub view tests

---
+++ notorhot/_tests/views.py
@@ -0,0 +1,28 @@
+import datetime
+from mock import Mock, patch
+
+from django.test import TestCase
+from django.forms import ValidationError
+
+from notorhot._tests.factories import mixer
+from notorhot.models import CandidateCategory, Candidate, Competition
+from notorhot.forms import VoteForm
+
+class CompetitionViewTestCase(TestCase):
+    pass
+
+
+class VoteViewTestCase(TestCase):
+    pass
+
+
+class CandidateViewTestCase(TestCase):
+    pass
+
+
+class LeaderboardViewTestCase(TestCase):
+    pass
+
+
+class CategoryListVIewTestCase(TestCase):
+    pass
--- notorhot/tests.py
+++ notorhot/tests.py
@@ -3,3 +3,5 @@ from notorhot._tests.models import NotorHotCategoryTestCase, \
     NotorHotCandidateTestCase, NotorHotCompetitionTestCase
     
 from notorhot._tests.forms import NotorHotVoteFormTestCase
+from notorhot._tests.views import CompetitionViewTestCase, VoteViewTestCase, \
+    CandidateViewTestCase, LeaderboardViewTestCase, CategoryListVIewTestCase
